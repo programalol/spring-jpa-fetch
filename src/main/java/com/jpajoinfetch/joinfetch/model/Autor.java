a6aacf68ab76abdb18813fddff29830e6c8b7909
jeisscor <59752118+santiago-cortes-ortiz@users.noreply.github.com>
2022-11-24 23:09:57 +0000

    uwu

--- src/main/java/com/jpajoinfetch/joinfetch/model/Autor.java
+++ src/main/java/com/jpajoinfetch/joinfetch/model/Autor.java
@@ -1,9 +1,19 @@
 package com.jpajoinfetch.joinfetch.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import java.util.Set;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.Id;
+
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;;
 
 
@@ -11,7 +21,20 @@ import lombok.Setter;;
 @Table(name="autores")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Autor {
+    @Id
+    @Column(name="id_autor")
     private Long id;
     private String nombre;
+    private String apellido;
+    private String nacionalidad;
+    @ManyToMany
+    @JoinTable(
+    name = "autores_libros", 
+    joinColumns = @JoinColumn(name = "id_autor"), 
+    inverseJoinColumns = @JoinColumn(name = "id_libro")
+    )
+    Set<Libro> libros;
 }
--- src/main/java/com/jpajoinfetch/joinfetch/model/Libro.java
+++ src/main/java/com/jpajoinfetch/joinfetch/model/Libro.java
@@ -1,5 +1,27 @@
 package com.jpajoinfetch.joinfetch.model;
+import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;;
+
+@Entity
+@Table(name="libros")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Libro {
+    @Id
+    @Column(name="id_libro")
+    private Long id;
+    private String titulo;
+    private LocalDate fechaPublicacion;
     
 }
