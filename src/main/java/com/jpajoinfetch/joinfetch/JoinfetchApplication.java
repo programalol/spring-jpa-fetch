223df42827842da78403e2993e44af7ab2ebced4
jeiss <jeissoncortes02@outlook.com>
2022-11-23 22:32:16 -0500

    push

---
+++ src/main/java/com/jpajoinfetch/joinfetch/JoinfetchApplication.java
@@ -0,0 +1,13 @@
+package com.jpajoinfetch.joinfetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinfetchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinfetchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpajoinfetch/joinfetch/JoinfetchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpajoinfetch.joinfetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinfetchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
